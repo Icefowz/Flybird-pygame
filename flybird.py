4ba2e0b62c7737f8feccdc99d7086c8d5fc82bf6
tzeno <nicktznlr2@gmail.com>
2025-11-18 13:50:37 +0200

    initial commit

---
+++ flybird.py
@@ -0,0 +1,50 @@
+from turtle import width
+import pygame
+import random
+pygame.init()
+
+
+WIDTH = 400
+HEIGHT = 600
+
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Flappy bird")
+color = (0,0,0)
+clock = pygame.time.Clock()
+
+
+#-------------------------------LOAD IMAGE -----------------------------------
+
+bird_image = pygame.image.load("C:/Users/User/Desktop/Flybird-pygame/bird1  .png").convert_alpha()
+bird_image = pygame.transform.scale(bird_image, (34, 24))
+
+bird_sprites = [bird_image]
+
+pipe_image = pygame.image.load("C:/Users/User/Desktop/Flybird-pygame/pipe.png").convert_alpha() # adjust resolution 
+pipe_width = 70 # width of pipes
+pipe_gap = 50
+
+
+#------------------------GAME FUNCTION--------------------------------
+
+
+
+def create_pipe():
+    height_top = random.randint(50, HEIGHT - pipe_gap - 50)
+    pipe_top = pygame.Rect(WIDTH, 0, pipe_width, height_top)
+    pipe_bottom = pygame.rect(WIDTH, height_top, + pipe_gap,
+    pipe_width, HEIGHT - (height_top + pipe_gap))
+    return(pipe_top, pipe_bottom)
+
+
+
+running = True 
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+    clock.tick(5)
+pygame.quit()
